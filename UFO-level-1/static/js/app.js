82f5d5fd7b467b5b41ee70e79ef50417465726f2
Nicholas Burns <nmburns@triad.rr.com>
2020-12-14 18:31:05 -0500

    edits

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,22 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+console.log(tableData)
+
+// get references
+var tbody = d3.select('tbody');
+var columns = ['datetime', "city", "state", "country", "shape", "durationMinutes", "comments"];
+
+// populate html with data from the table
+var ufoData = (dataInput) => {
+    dataInput.forEach(ufoSightings => {
+        var row = tbody.append("tr");
+        columns.forEach(column => row.append("td").text(ufoSightings[column]))
+        
+    });
+}
+
+ufoData(tableData);
+
+
+var button = d3.select("#filter-btn");
