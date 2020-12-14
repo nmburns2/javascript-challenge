080f986831016ee79ae9f28b77cd059a173b9f06
Nicholas Burns <nmburns@triad.rr.com>
2020-12-14 18:55:40 -0500

    add comments

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -3,7 +3,7 @@ var tableData = data;
 
 console.log(tableData)
 
-// get references
+// get references to the table
 var tbody = d3.select('tbody');
 var columns = ['datetime', "city", "state", "country", "shape", "durationMinutes", "comments"];
 
@@ -20,21 +20,25 @@ ufoData(tableData);
 
 // select the filter button
 var button = d3.select("#filter-btn");
-
 var form = d3.select("form");
 
+// create event handler
+// set up filter button
 button.on("click", search)
 form.on("submit", search)
 
+// create event handler function
 function search() {
-
+    // prevent page from refreshing
     d3.event.preventDefault();
+    
     var tableDate = d3.select("#datetime");
     var value = tableDate.property("value");
     var filtered = tableData.filter(tableData => tableData.datetime === value);
     var sightingData = d3.selectAll('tbody>tr');
     sightingData.html("");
     let response = {filtered};
+    // create condition if filtered date doesnt exist
     if (response.filtered.length !== 0) {
         ufoData(filtered);
     }
